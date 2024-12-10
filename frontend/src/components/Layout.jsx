3e2ce2e23d141e814faf7a0166fea6df5cbe740e
Renelle <rq@HYMacBookPro.local>
2024-12-09 17:53:31 -0800

    add layout.jsx and background

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import "../styles/Layout.css";
 
+const Layout = ({ title, onSearch, children }) => {
+  return (
+    <div className="layout-container">
+      <Header title={title} onSearch={onSearch} />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
