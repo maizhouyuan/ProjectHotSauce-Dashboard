1ffaa9583f93df362ce3e9d8d0e8e7b172df94e1
WAYNE_SEA <zhou.zhiwe@northeastern.edu>
2025-03-27 13:37:00 -0700

    update qmd file

--- backend/temp/data.py
+++ backend/temp/data.py
@@ -2,20 +2,20 @@
 report_type = "comparison"
 selected_sensors = [
   {
-    "value": "d8bfc0c0e514",
-    "label": "Sensor 5 - Courtyard"
-  },
-  {
     "value": "fcf5c497654a",
     "label": "Sensor 3 - Main Event Space"
   },
   {
-    "value": "08f9e05fd2d3",
-    "label": "Sensor 8 - Room 210"
+    "value": "bcff4dd3b442",
+    "label": "Sensor 4 - Room 220"
+  },
+  {
+    "value": "a4cf12ff89ae",
+    "label": "Sensor 6 - Room 216"
   }
 ]
-start_time = "8:00"
-end_time = "14:30"
+start_time = "6:00"
+end_time = "11:59"
 date_range = {
   "startDate": "2025-03-26T07:00:00.000Z",
   "endDate": "2025-03-27T06:59:59.999Z",
