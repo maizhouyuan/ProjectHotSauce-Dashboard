7d09249b36d22f1876bbc014f7dd97f5509f1f9a
WAYNE_SEA <zhou.zhiwe@northeastern.edu>
2025-03-27 14:02:18 -0700

    fixed report header date. changed index title and icon

--- backend/temp/data.py
+++ backend/temp/data.py
@@ -6,16 +6,16 @@ selected_sensors = [
     "label": "Sensor 3 - Main Event Space"
   },
   {
-    "value": "bcff4dd3b442",
-    "label": "Sensor 4 - Room 220"
-  },
-  {
     "value": "a4cf12ff89ae",
     "label": "Sensor 6 - Room 216"
+  },
+  {
+    "value": "40f52032b5b7",
+    "label": "Sensor 7 - Staff Workspace"
   }
 ]
-start_time = "6:00"
-end_time = "11:59"
+start_time = "0:00"
+end_time = "7:59"
 date_range = {
   "startDate": "2025-03-26T07:00:00.000Z",
   "endDate": "2025-03-27T06:59:59.999Z",
